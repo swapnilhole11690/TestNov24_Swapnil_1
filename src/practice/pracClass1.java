c6df72e170d037c9830030fb7de7f4a9d4807723
Hole <swapnil.hole@t-systems.com>
2024-12-14 11:35:41 +0530

    change in A

--- src/practice/pracClass1.java
+++ src/practice/pracClass1.java
@@ -3,8 +3,7 @@ package practice;
 public class pracClass1 {
 	public static void main(String[] args) {
 
-		System.out.println("Hello_class1 Branch A changes");
-
+		System.out.println("Hello_class1 Branch A changes,branch B changes");
 		System.out.println("Branch B changes");
 
 	}
