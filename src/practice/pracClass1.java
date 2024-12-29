5eec3f3a68be27d42da330c913f7a676709faf45
Hole <swapnil.hole@t-systems.com>
2024-12-29 12:13:13 +0530

    one

--- src/practice/pracClass1.java
+++ src/practice/pracClass1.java
@@ -5,6 +5,7 @@ public class pracClass1 {
 	public static void main(String[] args) {
 	    System.out.println("Hello_class1 ");
 	    System.out.println("Hello_class1_1change ");
+	    System.out.println("1");
 
 	}
 }
